4806267bdb89549b0b0819db413aa7e8f2758daf
chethanajain <chethanajain.87@gmail.com>
2021-11-24 19:17:24 +0530

    new cypress files

---
+++ cypress/integration/cs.js
@@ -0,0 +1,31 @@
+describe("Computer database",()=>{
+
+
+    it("Click on Add Computer",()=>{
+    
+    cy.visit("https://computer-database.herokuapp.com/computers")
+    cy.get('a#add').click()
+   var name = "ASCI"
+cy.get('input#name').type(name)
+cy.get('input#introduced').type("2021-11-22")
+cy.get('input#discontinued').type("2022-11-22")
+cy.get('select#company').select('Thinking Machines').should('have.value','2')
+cy.get('input[type="submit"]').click()
+cy.wait(5000)
+cy.get('input#searchbox').type(name)
+cy.wait(5000)
+cy.get('input#searchsubmit').click()
+cy.wait(10000)
+//cy.get("td")
+cy.get("tbody > tr > :nth-child(1)").eq(0).contains("a",name).click()
+cy.wait(10000)
+cy.get('a[href="/computers"]').click() 
+
+
+
+  })
+
+
+})
+
+    
